6ae05e6213c5f72e2317dca3cac7f7a2e92e444f
NUhlarik <42522090+NUhlarik@users.noreply.github.com>
2021-08-28 16:35:32 -0400

    Add files via upload

---
+++ Pot_LED.ino
@@ -0,0 +1,14 @@
+const int ledPin = 5;
+const int potPin = A0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int potValue = analogRead(potPin);
+  int ledValue = map(potValue, 1023, 0, 0, 255);
+  analogWrite(ledPin, ledValue);
+}
